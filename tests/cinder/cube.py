b3336531f073ff97161a66ec7a756f823f8eb27e
Ed <eddeighton@hotmail.co.uk>
2019-05-20 19:02:04 +0100

    pondering how to integrate resumption criteria with python and c++ coroutines

--- tests/cinder/cube.py
+++ tests/cinder/cube.py
@@ -4,6 +4,14 @@ import pyeg
 
 r = pyeg.get_root()
 
-for i in range( 9 ):
-    r.CreateCube();
-    pyeg.sleep_seconds( 0.5 )
+pyeg.sleep_seconds( 0.01 )
+        
+for y in range( 10 ):
+    for i in range( 9 ):
+        r.CreateCube();
+        pyeg.sleep_seconds( 0.05 )
+        
+    for c in r.Cube.Range():
+        c.Stop()
+        pyeg.sleep_seconds( 0.05 )
+        
