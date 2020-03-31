2a9940dddf602009793782ca640224a4cf130cdd
Ed <eddeighton@hotmail.co.uk>
2020-03-31 15:38:20 +0100

    Started work on complete rework of build system

--- cmake/eg_runtime/eg_runtime_include.cmake
+++
@@ -1,8 +0,0 @@
-
-
-cmake_minimum_required(VERSION 3.14.2)
-
-
-function( link_eg_runtime targetname )
-	target_link_libraries( ${targetname} eg_runtime )
-endfunction( link_eg_runtime )
--- cmake/schema/schema_include.cmake
+++
@@ -1,9 +0,0 @@
-
-
-cmake_minimum_required(VERSION 3.14.2)
-
-include_directories( ${EG_ROOT_DIR}/src/schema )
-
-function( link_schemalib targetname )
-	target_link_libraries( ${targetname} schemalib )
-endfunction( link_schemalib )
